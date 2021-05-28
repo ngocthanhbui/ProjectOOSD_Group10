56b98123b8b48f3f3b8ef7ea5bf59ed69432565f
ngocthanhbui <72994444+ngocthanhbui@users.noreply.github.com>
2021-05-28 22:51:52 +0700

    Add files via upload

---
+++ Final Report/Design Pattern Code Demo/AbstractFactoryPattern.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        /// <summary>
+
+        /// MainApp startup class for Real-World
+
+        /// Abstract Factory Design Pattern.
+
+        /// </summary>
+
+        class MainApp
+
+        {
+            /// <summary>
+
+            /// Entry point into console application.
+
+            /// </summary>
+
+            public static void Main()
+            {
+                // Create and run the Excerise 
+
+                ContinentFactory Excecise = new ExceciseFactory();
+                UsersWorld world = new UsersWorld(Excecise);
+                world.RunFavorExcerciseChain();
+
+                // Create and run the UsersWorld
+
+                ContinentFactory Users = new UsersFactory();
+                world = new UsersWorld(Users);
+                world.RunFavorExcerciseChain();
+
+                // Wait for user input
+
+                Console.ReadKey();
+            }
+        }
+
+
+        /// <summary>
+
+        /// The 'AbstractFactory' abstract class
+
+        /// </summary>
+
+        abstract class ContinentFactory
+
+        {
+            public abstract Excercise CrFavoriteExceciseeExcercise();
+            public abstract User CrFavoriteExceciseeUser();
+        }
+
+        /// <summary>
+
+        /// The 'ConcreteFactory1' class
+
+        /// </summary>
+
+        class ExceciseFactory : ContinentFactory
+
+        {
+            public override Excercise CrFavoriteExceciseeExcercise()
+            {
+                return new Excercise1();
+            }
+            public override User CrFavoriteExceciseeUser()
+            {
+                return new User1();
+            }
+        }
+
+        /// <summary>
+
+        /// The 'ConcreteFactory2' class
+
+        /// </summary>
+
+        class UsersFactory : ContinentFactory
+
+        {
+            public override Excercise CrFavoriteExceciseeExcercise()
+            {
+                return new Excercise2();
+            }
+            public override User CrFavoriteExceciseeUser()
+            {
+                return new User2();
+            }
+        }
+
+        /// <summary>
+
+        /// The 'AbstractProductA' abstract class
+
+        /// </summary>
+
+        abstract class Excercise
+
+        {
+        }
+
+        /// <summary>
+
+        /// The 'AbstractProductB' abstract class
+
+        /// </summary>
+
+        abstract class User
+
+        {
+            public abstract void FavoriteExcecise(Excercise h);
+        }
+
+        /// <summary>
+
+        /// The 'ProductA1' class
+
+        /// </summary>
+
+        class Excercise1 : Excercise
+
+        {
+        }
+
+        /// <summary>
+
+        /// The 'ProductB1' class
+
+        /// </summary>
+
+        class User1 : User
+
+        {
+            public override void FavoriteExcecise(Excercise h)
+            {
+                // FavoriteExcecise Excercise1
+
+                Console.WriteLine(this.GetType().Name +
+                  " love the " + h.GetType().Name);
+            }
+        }
+
+        /// <summary>
+
+        /// The 'ProductA2' class
+
+        /// </summary>
+
+        class Excercise2 : Excercise
+
+        {
+        }
+
+        /// <summary>
+
+        /// The 'ProductB2' class
+
+        /// </summary>
+
+        class User2 : User
+
+        {
+            public override void FavoriteExcecise(Excercise h)
+            {
+                // FavoriteExcecise Excercise2
+
+                Console.WriteLine(this.GetType().Name +
+                  " love the " + h.GetType().Name);
+            }
+        }
+
+        /// <summary>
+
+        /// The 'Client' class 
+
+        /// </summary>
+
+        class UsersWorld
+
+        {
+            private Excercise _Excercise;
+            private User _User;
+
+            // Constructor
+
+            public UsersWorld(ContinentFactory factory)
+            {
+                _User = factory.CrFavoriteExceciseeUser();
+                _Excercise = factory.CrFavoriteExceciseeExcercise();
+            }
+
+            public void RunFavorExcerciseChain()
+            {
+                _User.FavoriteExcecise(_Excercise);
+            }
+        }
+    }
+}
---
+++ Final Report/Design Pattern Code Demo/Visitor.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+namespace User
+{
+    namespace VisitorDesignPattern
+    {
+        public interface IElement
+        {
+            void Accept(IVisitor visitor);
+        }
+    }
+    namespace VisitorDesignPattern
+    {
+        public class FriendAccout : IElement
+        {
+            public string FriendAccoutName { get; set; }
+
+            public FriendAccout(string name)
+            {
+                FriendAccoutName = name;
+            }
+
+            public void Accept(IVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+    }
+    namespace VisitorDesignPattern
+    {
+        public interface IVisitor
+        {
+            void Visit(IElement element);
+        }
+    }
+
+    namespace VisitorDesignPattern
+    {
+        public class User : IVisitor
+        {
+            public string Name { get; set; }
+            public User(string name)
+            {
+                Name = name;
+            }
+
+            public void Visit(IElement element)
+            {
+                FriendAccout FriendAccout = (FriendAccout)element;
+                Console.WriteLine("User: " + this.Name + " did a check up on " + FriendAccout.FriendAccoutName);
+            }
+        }
+    }
+
+     namespace VisitorDesignPattern
+    {
+        class MainUser : IVisitor
+        {
+            public string Name { get; set; }
+            public MainUser(string name)
+            {
+                Name = name;
+            }
+            public void Visit(IElement element)
+            {
+                FriendAccout FriendAccout = (FriendAccout)element;
+                Console.WriteLine("MainUser: " + this.Name + " like something on "
+                                + FriendAccout.FriendAccoutName + " account page");
+            }
+        }
+    }
+    
+    namespace VisitorDesignPattern
+    {
+        public class Friendlist
+        {
+            private static List<IElement> elements;
+            static Friendlist()
+            {
+                elements = new List<IElement>
+            {
+                new FriendAccout("Friend2"),
+                new FriendAccout("Friend3"),
+                new FriendAccout("Friend4")
+            };
+            }
+            public void PerformOperation(IVisitor visitor)
+            {
+                foreach (var FriendAccout in elements)
+                {
+                    FriendAccout.Accept(visitor);
+                }
+            }
+        }
+    }
+
+namespace VisitorDesignPattern
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Friendlist Friendlist = new Friendlist();
+                var visitor1 = new User("User 01");
+                Friendlist.PerformOperation(visitor1);
+                Console.WriteLine();
+                var visitor2 = new MainUser("John");
+                Friendlist.PerformOperation(visitor2);
+                Console.Read();
+            }
+        }
+    }
+}
+
+
